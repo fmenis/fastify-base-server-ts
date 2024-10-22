7fecb53d3c6cc95f9a3c33734a1a4a7d2976c241
Filippo Menis <filippomeniswork@gmail.com>
2024-10-22 23:17:21 +0200

    add pino, improve server options

--- src/utils/serverOpts.ts
+++ src/utils/serverOpts.ts
@@ -1,14 +1,42 @@
 import { FastifyServerOptions } from 'fastify'
+import pino, { stdTimeFunctions, LoggerOptions, Logger } from 'pino'
 
 export function buildServerOptions(): FastifyServerOptions {
   return {
-    logger: {
-      level: process.env.LOG_LEVEL,
-    },
+    loggerInstance: buildLoggerInstance(),
     ajv: {
       customOptions: {
         allErrors: true,
+        removeAdditional: false,
+        // coerceTypes: 'array',
+        // useDefaults ##TODO see if useful https://ajv.js.org/options.html#usedefaults
       },
     },
   }
+}
+
+function buildLoggerInstance(): Logger {
+  const loggerOptions: LoggerOptions = {
+    level: process.env.LOG_LEVEL,
+    timestamp: () => stdTimeFunctions.isoTime(),
+    formatters: {
+      level(label) {
+        return { level: label }
+      },
+      bindings() {
+        return { pid: undefined, hostname: undefined }
+      },
+    },
+    redact: {
+      paths: [
+        'password',
+        'oldPassword',
+        'newPassword',
+        'newPasswordConfirmation',
+      ],
+      censor: '**GDPR COMPLIANT**',
+    },
+  }
+
+  return pino(loggerOptions)
 }
